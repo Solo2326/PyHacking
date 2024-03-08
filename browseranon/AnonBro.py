010036e3bcb73987ee111048facba697ffe9c2ab
Solo2326 <162106343+Solo2326@users.noreply.github.com>
2024-03-08 13:37:54 +0100

    aggiornamento codice

--- browseranon/AnonBro.py
+++ browseranon/AnonBro.py
@@ -1,32 +1,36 @@
 import socket
 import socks
-import urllib2
-import mechanize
+import requests
+from bs4 import BeautifulSoup
 
 def connectTOR():
     socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050, True)
     socket.socket = socks.socksocket
 
+def analyze_form(form):
+    form_data = {'name': form.name, 'controls': []}
+    for control in form.controls:
+        control_dict = {
+            'name': control.name or "",
+            'type': control.type or "",
+            'value': control.value or ""
+        }
+        form_data['controls'].append(control_dict)
+    return form_data
+
 if __name__ == "__main__":
     connectTOR()
-    browser = mechanize.Browser()
-    browser.open("http://www.google.com")
-    try:
-        for form in browser.forms():
-            print("[*] Form Name %s " %(form.name))
-            for control in form.controls:
-                controlName = control.name
-                controlValue = control.value
-                controlType = control.type
-                if controlValue == None:
-                    controlValue = ""
-                if controlName == None:
-                    controlName = ""
-                if controlType == None:
-                    controlType = ""
-                print("[*][*] Control Name: %s " %(str(controlName)))
-                print("[*][*] Control Type: %s " %(str(controlType)))
-                print("[*][*] Control Value: %s " %(str(controlValue)))
+    response = requests.get("http://www.google.com")
+    response.raise_for_status()  # Raise error if there's a problem
 
-    except AttributeError:
-        pass 
+    soup = BeautifulSoup(response.content, 'html.parser')
+    forms = soup.find_all('form')
+
+    for form in forms:
+        form_data = analyze_form(form)
+        print("[*] Form:", form_data['name'])
+        for control in form_data['controls']:
+            print(f"   [*] Control Name: {control['name']}")
+            print(f"   [*] Control Type: {control['type']}")
+            print(f"   [*] Control Value: {control['value']}")
+
